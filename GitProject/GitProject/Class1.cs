78e0e962ac7739118fcc4e9b8e5992e0cd4b537b
Leon <Leonkwin@live.co.uk>
2016-03-08 16:35:57 +0000

    Uploaded a test project
    Sup!

---
+++ GitProject/GitProject/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GitProject
+{
+
+    public class Class1
+    {
+        private string myName = "N/A";
+        private int myAge = 0;
+
+        public string Name
+        {
+            get
+            {
+                return myName;
+            }
+            set
+            {
+                myName = value;
+            }
+        }
+
+        public int Age
+        {
+            get{  return myAge;}
+            set{ myAge = value;}
+        }
+    }
+}
---
+++ GitProject/GitProject/Index.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="GitProject.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Repository Push Project</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <h1>SecondRand & ThirdRand </h1>
+        Textbox <input type ="text"name="textbox" /><br />
+        <input type="button" name="button" value="Please press me!" />
+    </div>
+    </form>
+</body>
+</html>
